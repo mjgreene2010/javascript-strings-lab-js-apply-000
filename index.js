f359aaab8406c1a3fe4c4bc7253e84dfabe75cc8
mjgreene2010 <mjgreene2010@gmail.com>
2018-09-24 12:25:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
+<<<<<<< HEAD
 var greetSpecialGuest = `Hello, ` + `${specialGuest}` + `!`;
+=======
+var greetSpecialGuest = `Hello, ${specialGuest}`;
+>>>>>>> 3d5b7917a9bc8467c1308fe1c2d7b7a059f1ed10
 
 var topic = "space";
 
